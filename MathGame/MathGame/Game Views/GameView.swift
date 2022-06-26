ad152888717a4334da9b0044b80c83c4eb7f8f06
Sam-1607 <Samuel.Hiatt4295@stu.mtec.edu>
2022-06-25 18:32:13 -0600

    All Alerts Done for math game

--- MathGame/MathGame/Game Views/GameView.swift
+++ MathGame/MathGame/Game Views/GameView.swift
@@ -16,6 +16,19 @@ struct GameView: View {
     @State var incorrectAnswered = 0
     @State var heartsFull: [String] = ["heart.fill", "heart.fill","heart.fill"]
     @State var gameInfo: [Int] 
+    @State var quitButtonTitleText = "Quit"
+    @State var isShowing = false
+    @State var answeredAmount = 0
+    @State var endAlertIsShowing = false
+    @State var quitButtonBackground = Color.red
+    @State var quitButtonForeground = Color.yellow
+    @State var quitButtonSystemImage = "clear"
+    @State var overAchieverAlert = "Are you sure you want to Quit?"
+    @State var continueAlertButtonText = "Nevermind"
+    @State var lostAlertShowing = false
+    @State var hasWon = false
+    @State var lostAlertText = "You have run out of lives, feel free to switch the settings and try again"
+    @State var lostAlertButtonText = "Go Home 😞"
     
     var questionCount: Int
     var aduioPlayer = SoundPlayer()
@@ -98,21 +111,32 @@ struct GameView: View {
                     
                     HStack {
                         Button {
-                            
+                            isShowing.toggle()
                         } label: {
                             VStack {
-                                Label("", systemImage: "clear")
+                                Label("", systemImage: quitButtonSystemImage)
                                     .foregroundColor(.yellow)
                                     .font(.system(size: 40))
                                     .padding(.leading, 55)
                                     .padding(.trailing, 55)
-                                Text("Quit")
-                                    .foregroundColor(.yellow)
+                                Text("\(quitButtonTitleText)")
+                                    .foregroundColor(quitButtonForeground)
                                     .padding(.trailing, 8)
                             }
                             .padding(5)
-                            .background(.red)
+                            .background(quitButtonBackground)
                             .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .alert(overAchieverAlert, isPresented: $isShowing) {
+                                Button(continueAlertButtonText, role: .cancel) {
+                                    
+                                }
+                                .padding()
+                                .background(.blue)
+                                .clipShape(RoundedRectangle(cornerRadius: 15))
+                                Button("Quit", role: .destructive) {
+                                    dismiss()
+                                }
+                            }
                         }
                         
                         Button {
@@ -130,6 +154,19 @@ struct GameView: View {
                     }
                     .padding(.top, 50)
                     .padding(.bottom)
+                    .alert("Nice Job You finsihed all \(questionCount) Questions!!", isPresented: $endAlertIsShowing) {
+                        Button("Home") {
+                            
+                        }
+                        Button("Contine Playing") {
+                            
+                        }
+                    }
+                    .alert(lostAlertText, isPresented: $lostAlertShowing) {
+                        Button(lostAlertButtonText) {
+                            dismiss()
+                        }
+                    }
                     
                 }
             }
@@ -144,6 +181,18 @@ struct GameView: View {
         if userAnswer == correctAnser {
             self.aduioPlayer.playCorrectSound(soundName: "correct", soundType: "mp3")
             self.gameInfo.shuffle()
+            self.answeredAmount += 1
+            self.userAnswer = ""
+            if answeredAmount ==  questionCount {
+                self.hasWon = true
+                self.endAlertIsShowing.toggle()
+                self.quitButtonSystemImage = "house.fill"
+                self.quitButtonForeground = .white
+                self.quitButtonBackground = .blue
+                self.quitButtonTitleText = "Home"
+                self.continueAlertButtonText = "Keep Playing 🤩"
+                self.overAchieverAlert = "Nice Job you sure are an overachiver WOW!!"
+            }
             
         } else {
             self.lives -= 1
@@ -153,9 +202,14 @@ struct GameView: View {
                 self.heartsFull[2] = "heart"
             } else if lives == 1 {
                 self.heartsFull[1] = "heart"
-            } else if lives == 0 {
-                //Trigger some alert
+            } else if lives == 0 && self.hasWon == false {
                 self.heartsFull[0] = "heart"
+                self.lostAlertShowing.toggle()
+            } else if lives == 0 && self.hasWon == true {
+                self.heartsFull[0] = "heart"
+                self.lostAlertText = "Nice Job anways, feel free to crank up the settings to give yourself a bit of a challenge 👍"
+                self.lostAlertButtonText = "Home"
+                self.lostAlertShowing.toggle()
             }
         }
     }
